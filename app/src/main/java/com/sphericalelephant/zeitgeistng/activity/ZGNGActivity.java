290e8579453c34f81cbb714f1041905bcc3c426d
Patrick Sturm <p.sturm@spherical-elephant.com>
2016-02-16 11:18:34 +0100

    dummy drawertoggle hamburger menu added

--- app/src/main/java/com/sphericalelephant/zeitgeistng/activity/ZGNGActivity.java
+++ app/src/main/java/com/sphericalelephant/zeitgeistng/activity/ZGNGActivity.java
@@ -1,7 +1,13 @@
 package com.sphericalelephant.zeitgeistng.activity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 
@@ -10,17 +16,34 @@ import com.sphericalelephant.zeitgeistng.fragment.imagegrid.ImageGridFragment;
 
 import at.diamonddogs.ui.annotation.UiAnnotationProcessor;
 import at.diamonddogs.ui.receiver.IndeterminateProgressControl;
-import butterknife.Bind;
-import butterknife.ButterKnife;
 
 public class ZGNGActivity extends AppCompatActivity implements IndeterminateProgressControl {
-	@Bind(R.id.activity_zgngactivity_pb_progress)
-	ProgressBar progressBar;
+	private ProgressBar progressBar;
+	private DrawerLayout drawerLayout;
+
+	private ActionBarDrawerToggle drawerToggle;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_zgngactivity);
+
+		Toolbar toolbar = (Toolbar) findViewById(R.id.activity_zgngactivity_tb_tabs);
+		setSupportActionBar(toolbar);
+
+		drawerLayout = (DrawerLayout) findViewById(R.id.activity_zgngactivity_dl_drawerlayout);
+		progressBar = (ProgressBar) findViewById(R.id.activity_zgngactivity_pb_progress);
+
+		drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.general_openhamburger, R.string.general_closehamburger);
+
+		drawerLayout.setDrawerListener(drawerToggle);
+		
+		ActionBar ab = getSupportActionBar();
+
+		if (ab != null) {
+			ab.setDisplayHomeAsUpEnabled(true);
+			ab.setHomeButtonEnabled(true);
+		}
 
 		if (getSupportFragmentManager().findFragmentById(R.id.activity_zgngactivity_fl_container) == null) {
 			getSupportFragmentManager()
@@ -31,15 +54,20 @@ public class ZGNGActivity extends AppCompatActivity implements IndeterminateProg
 	}
 
 	@Override
-	protected void onResume() {
-		super.onResume();
-		ButterKnife.bind(this);
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		drawerToggle.syncState();
 	}
 
 	@Override
-	protected void onPause() {
-		super.onPause();
-		ButterKnife.unbind(this);
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		drawerToggle.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		return drawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
 	}
 
 	@Override
