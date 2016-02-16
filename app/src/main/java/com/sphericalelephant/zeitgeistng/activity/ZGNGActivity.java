c42d7e47735d377a44e9f53ddc311a876920031f
Patrick Sturm <p.sturm@spherical-elephant.com>
2016-02-16 10:35:10 +0100

    added drawerlayout and toolbar stuff

--- app/src/main/java/com/sphericalelephant/zeitgeistng/activity/ZGNGActivity.java
+++ app/src/main/java/com/sphericalelephant/zeitgeistng/activity/ZGNGActivity.java
@@ -2,11 +2,20 @@ package com.sphericalelephant.zeitgeistng.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.sphericalelephant.zeitgeistng.R;
 import com.sphericalelephant.zeitgeistng.fragment.imagegrid.ImageGridFragment;
 
-public class ZGNGActivity extends AppCompatActivity {
+import at.diamonddogs.ui.annotation.UiAnnotationProcessor;
+import at.diamonddogs.ui.receiver.IndeterminateProgressControl;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class ZGNGActivity extends AppCompatActivity implements IndeterminateProgressControl {
+	@Bind(R.id.activity_zgngactivity_pb_progress)
+	ProgressBar progressBar;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -17,5 +26,37 @@ public class ZGNGActivity extends AppCompatActivity {
 				.beginTransaction()
 				.add(R.id.activity_zgngactivity_fl_container, ImageGridFragment.newInstance())
 				.commit();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		ButterKnife.bind(this);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		ButterKnife.unbind(this);
+	}
+
+	@Override
+	public void showIndeterminateProgress() {
+		progressBar.setVisibility(View.VISIBLE);
+	}
+
+	@Override
+	public void hideIndeterminateProgress() {
+		progressBar.setVisibility(View.INVISIBLE);
+	}
+
+	@Override
+	public boolean isIndeterminateProgressShowing() {
+		return progressBar.getVisibility() == View.VISIBLE;
+	}
+
+	@Override
+	public void processUiAnnotations(UiAnnotationProcessor uiAnnotationProcessor) throws IllegalAccessException, IllegalArgumentException {
+		uiAnnotationProcessor.process(this);
 	}
 }
